658d1cf9d5b47a1de6be719b46b8d65752e482b9
Ulaş Sancak <ulas.sancak@metus.com>
2016-11-16 18:54:28 +0300

    Initial Commit

---
+++ TweetSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TweetSearch
+//
+//  Created by Ulaş Sancak on 16/11/2016.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TweetSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TweetSearch
+//
+//  Created by Ulaş Sancak on 16/11/2016.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
