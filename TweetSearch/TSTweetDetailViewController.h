1f4be679ca36f265a31aee5b55c95b1a2d7ff7ea
Ulaş Sancak <ulas.sancak@metus.com>
2016-11-18 20:26:31 +0300

    Displaying tweet detail. Added 3D peeking and popping support.

---
+++ TweetSearch/TSTweetDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  TweetDetailViewController.h
+//  TweetSearch
+//
+//  Created by Ulaş Sancak on 18/11/2016.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TSTweet.h"
+
+@interface TSTweetDetailViewController : UIViewController
+
+@property (strong, nonatomic) TSTweet *tweet;
+
+@end
---
+++ TweetSearch/TSTweetDetailViewController.m
@@ -0,0 +1,53 @@
+//
+//  TweetDetailViewController.m
+//  TweetSearch
+//
+//  Created by Ulaş Sancak on 18/11/2016.
+//  Copyright © 2016 Ulaş Sancak. All rights reserved.
+//
+
+#import "TSTweetDetailViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
+@interface TSTweetDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *tweetScrollView;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *statusImageView;
+
+@end
+
+@implementation TweetDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self loadData];
+}
+
+- (void)loadData {
+    _usernameLabel.text = _tweet.user.twitterScreenName;
+    [_userImageView sd_setImageWithURL:[NSURL URLWithString:_tweet.user.profileImageUrlHttps]];
+    _statusLabel.text = _tweet.text;
+    TSMediaData *mediaData = _tweet.entities.media[0];
+    [_statusImageView sd_setImageWithURL:[NSURL URLWithString:mediaData.mediaThumbUrl]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
